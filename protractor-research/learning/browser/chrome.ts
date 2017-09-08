f6a4fa8863318e3d1236eb3142a0167be8dfea95
Nam Nhat Doan <namndoan@kms-technology.com>
2017-09-08 09:12:44 +0700

    remove learning

--- protractor-research/learning/browser/chrome.ts
+++
@@ -1,5 +0,0 @@
-import * as chrome from 'selenium-webdriver/chrome';
-
-const driver = chrome.Driver.createSession();
-driver.get("http://www.google.com");
-driver.quit();
--- protractor-research/learning/check.ts
+++
@@ -1,3 +0,0 @@
-enum Color{Red, Green, Blue}
-let color:String = Color[2]
-
